c8face5716ca08cb3c7400072e2e19b0df4e0fab
Lolikana <thomas.guiart@gmail.com>
2022-07-07 14:14:59 +0900

    add google map

--- components/templates/ConcertTemplate.tsx
+++ components/templates/ConcertTemplate.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import ConcertTop from "../organisms/concert/ConcertTop";
-
-import styles from "../../styles/templates/concert_template.module.scss";
 import ConcertArticlesTop from "../organisms/concert/ConcertArticlesTop";
 import ConcertReservation from "../organisms/concert/ConcertReservation";
 import ConcertInfoList from "../molecules/concert/ConcertInfoList";
+import AccessGoogleMap from "../organisms/AccessGoogleMap";
 import ImportantPoints from "../atoms/ImportantPoints";
 import Contact from "../organisms/Contact";
+
+import styles from "../../styles/templates/concert_template.module.scss";
 
 const ConcertTemplate: FC = () => {
   return (
@@ -15,6 +16,7 @@ const ConcertTemplate: FC = () => {
       <ConcertArticlesTop />
       <ConcertReservation />
       <ConcertInfoList />
+      <AccessGoogleMap showTitle={false} />
       <ImportantPoints />
       <Contact />
     </article>
