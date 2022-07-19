e814ca860d461fbf52aaa6e75bcaf2939625780b
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-07-19 14:32:40 +0900

    sample initialization

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from "next";
-import FaqItem from "../components/atoms/FaqItem";
-import FaqList from "../components/organisms/faq/FaqList";
-import { faqData } from "../lib/data";
 
-import styles from "../styles/organisms/faq_list.module.scss";
 
 const Sample: NextPage = () => {
   return (
     <section>
-      <FaqList faqData={faqData} />
+      Sample
     </section>
   );
 };
